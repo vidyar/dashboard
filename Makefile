bfccdd0c036d6e72f8fd06260f5bbe71888443da
sharathmq <sharath@shippable.com>
2013-11-06 21:10:11 +0530

    basic logging and supervisor

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 #TODO: Need to remove nyan reportter to export
 
+LOGDIR = logs
+run:
+	mkdir -p $(LOGDIR)
+	supervisor app.js
+
 TESTS = test/*.js
 test:
 	 ./node_modules/mocha/bin/mocha --timeout 5000 --reporter nyan $(TESTS)
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
-
+var logger = require('./logger');
 var app = exports.app = express();
 
 app.get('/', function(request, response) {
    response.send("Welcome to Shippable");
 });
 
-
+logger.info('Server started');
 app.listen(3000);
