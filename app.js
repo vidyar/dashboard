3f199a8481f0bb399a3643ee304ab09279638369
Swati <swati730@gmail.com>
2013-11-15 13:32:59 +0530

    Rendering html page

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var express = require('express');
 var logger = require('./logger');
 var app = exports.app = express();
 
+app.use(express.bodyParser());
 app.get('/', function(request, response) {
-   response.send("Welcome to Shippable");
+   response.sendfile('./views/index.html');
 });
-
+app.post('/',function(req,res){
+logger.info('Callback');
+var user=req.body.name,
+    pass=req.body.pass;
+    logger.info('User: '+user);
+res.end();
+});
 logger.info('Server started');
 app.listen(3000);
